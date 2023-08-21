6cbaf41d30e7a3148d00e0d995731964cad582da
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2023-08-21 22:22:07 +0100

    add mix

--- src/components/Header_Services/Header_Services.jsx
+++ src/components/Header_Services/Header_Services.jsx
@@ -4,24 +4,35 @@ import arrow from "../../../public/arrow.svg";
 
 const Header_Services = () => {
   return (
-    <div className="relative h-screen mx-auto bg-black pl-5 ">
-      <div className="arrow hidden lg:block bottom-20 p-5 bg-white rounded-full opacity-80 z-50 ">
-        <Link rel="stylesheet" href="#offer_services">
-          <Image
-            src={arrow}
-            height={45}
-            alt="icon arrow"
-            className="animate-bounce"
-          />
-        </Link>
-      </div>
+    <div className="relative h-screen mx-auto bg-black ">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="object-cover h-full w-full opacity-5"
+        poster="https://res.cloudinary.com/rga/video/upload/so_0,q_10/f_auto/q_auto/v1636982937/rga.com/RGA-WebHero-05_1_qs6x7a.jpg"
+      >
+        <source
+          src="https://res.cloudinary.com/rga/video/upload/f_auto/q_auto/v1636982937/rga.com/RGA-WebHero-05_1_qs6x7a.mp4"
+          type="video/mp4"
+        />
+        <source
+          src="https://res.cloudinary.com/rga/video/upload/f_auto/q_auto/v1636982937/rga.com/RGA-WebHero-05_1_qs6x7a.webm"
+          type="video/webm"
+        />
+        <source
+          src="https://res.cloudinary.com/rga/video/upload/f_auto/q_auto/v1636982937/rga.com/RGA-WebHero-05_1_qs6x7a.ogv"
+          type="video/ogv"
+        />
+      </video>
       <div className="hidden lg:flex items-center absolute top-0 right-0 text-center ">
         <h1 className=" uppercase font-heading text-5xl  text-white">
           <span>We design business</span> <br />
           <span>and brands for a more</span> <br /> <span>human future</span>
         </h1>
       </div>
-      <h1 className="text-white text-[130px] leading-[130px] lg:text-[250px] lg:leading-[200px] uppercase absolute bottom-10">
+      <h1 className=" pl-5 text-white text-[130px] leading-[130px] lg:text-[250px] lg:leading-[200px] uppercase absolute bottom-10">
         how <br /> we can <br /> help
       </h1>
     </div>
